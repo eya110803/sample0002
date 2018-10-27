110e34386c829363ac3e5882bcf0c58d2ce26c23
eya110803 <eya110803@gmail.com>
2018-10-27 16:46:25 +0900

    列幅を指定できるよう修正しました。
    列幅を指定できるよう修正。
    データを修正。

--- index.js
+++ index.js
@@ -6,26 +6,32 @@ function addTable() {
 	// データ
 	var data = [ {
 		r1c1 : "データr1c1",
-		r1c2 : 10,
+		r1c2 : "データr1c2",
 		r1c3 : "データr1c3",
+		r1c4 : "データr1c4",
 		r2c1 : "データr2c1",
-		r2c2 : 20,
+		r2c2 : "データr2c2",
 		r2c3 : "データr2c3",
+		r2c4 : "データr2c4",
 		r3c1 : "データr3c1",
-		r3c2 : 30,
+		r3c2 : "データr3c2",
 		r3c3 : "データr3c3",
+		r3c4 : "データr3c4",
 		rowspan1 : 3,
 		rowspan2 : 1
 	}, ];
 	var data2 = [ {
-		c2 : "データc2-1",
-		c3 : 10,
+		c2 : "データc2-1　長い文字列でも大丈夫？",
+		c4 : "データc4-1",
 	}, {
-		c2 : "データc2-2",
-		c3 : 20,
+		c2 : "データc2-2　長い文字列でも大丈夫？",
+		c4 : "データc4-2"
 	}, {
-		c2 : "データc2-3",
-		c3 : 30,
+		c2 : "データc2-3　長い文字列でも大丈夫？",
+		c4 : "データc4-3"
+	}, {
+		c2 : "データc2-4　長い文字列でも大丈夫？",
+		c4 : "データc4-4",
 	}, ];
 
 	// rowspanを調整する
@@ -37,4 +43,5 @@ function addTable() {
 
 	// idがresult-table1の要素に、データdata2を設定したテンプレートresult-table2を追加する
 	$.tmpl("result-table2", data2).appendTo("#result-table1");
+
 }
--- index.template.js
+++ index.template.js
@@ -2,30 +2,34 @@ $(function(){
 	// 必須の３行のテンプレートをkey="result-table1"として作る。
     $.template(
             "result-table1",
-        	"<table>\
-    			<tbody id=\"result-table1\">\
+        	'<table>\
+    			<tbody id="result-table1">\
     				<tr>\
-    					<td rowspan=${rowspan1}>${r1c1}</td>\
-    					<td>${r1c2}</td>\
-    					<td>${r1c3}</td>\
+    					<td class="c1" rowspan=${rowspan1}>${r1c1}</td>\
+    					<td class="c2" >${r1c2}</td>\
+    					<td class="c3" >${r1c3}</td>\
+            			<td class="c4" >${r1c4}</td>\
     				</tr>\
     				<tr>\
     					<td>${r2c2}</td>\
     					<td>${r2c3}</td>\
+            			<td>${r2c4}</td>\
     				</tr>\
     				<tr>\
-    					<td rowspan=${rowspan2}>${r3c2}</td>\
-    					<td>${r3c3}</td>\
+    					<td>${r3c2}</td>\
+    					<td rowspan=${rowspan2}>${r3c3}</td>\
+            			<td>${r3c4}</td>\
     				</tr>\
     			</tbody>\
-    		</table>"
+    		</table>'
     );
 
     // ４行目以降がある場合のテンプレートをkey="result-table2"として作る。
     $.template(
             "result-table2",
             "<tr>\
-            <td>${c3}</td>\
+            <td>${c2}</td>\
+            <td>${c4}</td>\
             </tr>"
     );
 });
